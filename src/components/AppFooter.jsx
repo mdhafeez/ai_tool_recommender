84f81478da20f20a6438b02fa11c5bbb47ed0db8
mohdhafeez <mohdhafeezj@gmail.com>
2026-06-11 22:40:59 +0800

    update footer appfooter.jsx

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,7 @@ const currentYear = 2026;
 export default function AppFooter({ translations }) {
   return (
     <footer className="app-footer">
-      <p>{translations.footer || `Copyright ${currentYear} CoE Latihan AI Sektor Awam`}</p>
+      <p>{translations.footer || `CoE Latihan AI Sektor Awam @ ${currentYear} | INTAN`}</p>
     </footer>
   );
 }
